baf8fc12d4c4bb9a46f73f20a71fcef546e7982d
Cas Rusnov <rusnovn@gmail.com>
2014-03-09 04:05:51 -0700

    logic2/ 	base.py: Modified event to include destination pin.                  Overridable pin base name in Base object.                  Pin comparitor function.                  Implement input event processing in Base.                  Base becomes ABC, raises Notimplement on execuet.                  Implement process outputs in Base.                  Create a addr_range function to produce pindefs for addressable objects.                  Add AddressableMixin for adding addressing input pins to other objects.                  Impelement NOT object using inverter map.         gate.py: Implement basic gates (AND, OR, NOR, NAND, XOR, XNOR using LUT base class. 	clock.py: Implement tickwise clock object. 	lut.py: Implement generic Look Up Table gate. 	mux.py: Implement generic Multiplexer/Demultiplexer gates. 	simulator.py: Unfinished container classes / WIP.   testlogic.py: Begun implementing basic tests for the new versions of logic simulator.

--- logic2/base.py
+++ logic2/base.py
@@ -1,17 +1,19 @@
-
-
 PIN_DIRECTION_IN = 0
-PIN_DIECTION_OUT = 1
+PIN_DIRECTION_OUT = 1
+
+import string
+
+invert = {0:1,1:0}
 
 class DeplicatePinError(Exception):
     pass
 
 class Event(object):
-    def __init__(self, time, value, origin, origin_pin):
-        self.value = value
-        self.processed = False
+    def __init__(self, time, value, origin, origin_pin, destination_pin):
+        self.value = int(value)
         self.origin = origin
         self.origin_pin = origin_pin
+        self.destination_pin = destination_pin
 
 class Pin(object):
     def __init__(self, pin_holder, pin_name):
@@ -19,8 +21,7 @@ class Pin(object):
         self.pin_name = pin_name
 
     def __eq__(self, other):
-        return (self.pin_holder == other.pin_holder) and
-               (self.pin_name == other.pin_name)
+        return (self.pin_holder == other.pin_holder) and (self.pin_name == other.pin_name)
 
     def __ne__(self, other):
         return not self.__eq__(other)
@@ -45,7 +46,6 @@ class Base(object):
             self.name = self._next_name()
 
         self.input_event_queue = list()
-        self.output_event_queue = list()
 
         self.pin_states = dict()
         self.next_pin_states = dict()
@@ -54,14 +54,16 @@ class Base(object):
 
         self.time = init_time
 
-    def _next_name(self):
+    def _next_name(self, basename=None):
         nr = self.__class__._number
         self.__class__._number += 1
-        return self.basename + str(nr)
+        if not basename:
+            basename=self.basename
+        return basename + str(nr)
 
 
-    def _add_pin(self, pin_name, pin_direction, init_state):
-        if self.pin_info.has_key(pin_name)
+    def _add_pin(self, pin_name, pin_direction, init_state=0):
+        if self.pin_info.has_key(pin_name):
             raise DuplicatePinError()
         self.pin_states[pin_name] = init_state
         self.next_pin_states[pin_name] = init_state
@@ -77,31 +79,63 @@ class Base(object):
         self.output_connections.append(newpin)
         newpin.send_event(Event(self.time, self.pin_states[pin_name], self, pin_name))
 
+    def pin_changed_p(self, pin_name):
+        return self.next_pin_states[pin_name] != self.pin_states[pin_name]
+
     def recv_event(self, event):
         self.input_event_queue.append(event)
 
     def process_inputs(self, time):
-        # iterate waiting events and update next input pin states based on this
-        pass
+        # update next_state based on input events.
+        for ev in self.input_event_queue:
+            if ev.time <= time:
+                if self.next_pin_states.has_key(ev.destination_pin):
+                    self.next_pin_states[ev.destination_pin] = ev.value
 
-    def execute(self, time, changed_pins):
+    def execute(self, time):
         # to be implemented by the gates in question (examine next input states and update output states based on differences).
-        pass
+        raise NotImplementedError()
 
     def process_outputs(self, time):
         # check all of the output pin states against the previous states and produce update events on differences
-        pass
+        for pin,value in self.next_pin_states.iteritems():
+            if self.pin_states[pin] != value:
+                for conn in self.output_connections[pin]:
+                    newev = Event(time, value, self, pin, conn.pin_name)
+                    conn.send_event(newev)
 
     def __repr__(self):
         return '<%s %s>' % (str(self.__class__.__name__), self.name)
 
-class Bus(Base):
-    basename='Bus'
+def addr_range(bits):
+    for addr in xrange(2**bits):
+        yield tuple([int(x) for x in tuple(string.rjust(bin(addr)[2:], bits, '0'))])
 
-class MUX(Base):
-    basename='MUX'
-    pass
+class AddressableMixin(object):
+    def __init__(self, address_count, selector_prefix='SEL'):
+        self._a_m_sel_pins = list()
+        self._a_m_curr_address = None
+        self._a_m_prefix = selector_prefix
 
-class LUT(Base):
-    basename='LUT'
-    pass
+        sel_count = int(math.ceil(math.log(address_count, 2)))
+
+        for i in range(sel_count):
+            self._add_pin('%s%d' % i, PIN_DIRECTION_OUT, 0)
+            self._a_m_sel_pins.append('%s%d' % i)
+
+    def get_address(self):
+        return int("".join([str(self.next_pin_states[p]) for p in self._a_m_sel_pins]), 2)
+
+class NOT(Base):
+    basename='NOT'
+
+    def __init__(self, name=None):
+        Base.__init__(self, name=name)
+        self._add_pin('IN', PIN_DIRECTION_IN)
+        self._add_pin('OUT',PIN_DIRECTION_OUT)
+
+        self.execute(0)
+        self.process_outputs(0)
+
+    def execute(self, time):
+        self.next_pin_states['OUT'] = invert[self.next_pin_states['IN']]
